df40844541472128063736061d7715e8e5c2eb1a
BradenRupp <you@example.com>
2014-10-06 00:16:03 -0500

    Added Containers and Calendar with events
    Added php for calendar info and css and js for the calendar. Need to
    figure out how to make all three columns an equal width

---
+++ libs/show_data.php
@@ -0,0 +1,18 @@
+[
+  {
+    "date":"2014-10-17",
+    "badge":true,
+    "title":"Calendar Test",
+    "body":"<p class=\"lead\">Party<\/p><p>Like it's 1999.<\/p>",
+    "footer":"Footer 1",
+    "classname":"purple-event"
+  },
+  {
+    "date":"2014-10-28",
+    "badge":true,
+    "title":"Calendar Test #2",
+    "body":"<p class=\"lead\">What?<\/p><p>Another Test of the site 2<\/p>",
+    "footer":"Footer 2",
+    "classname":"purple-event"
+  }
+]
